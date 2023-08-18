ec2fdd245a1d825876fde29febdbea3576a9a79c
Kristbooker <boybook123@gmail.com>
2023-08-18 21:57:09 +0700

    update comment file

--- CarNBrand.py
+++ CarNBrand.py
@@ -5,7 +5,9 @@ import numpy
 import requests  
 import os
 
+#hog
 def img2vec(img):
+    #นำภาพมาแปลงเป็น base64แล้วส่งไปที่ endpoint api/genhog
     resized = cv2.resize(img,(128,128), cv2.INTER_AREA)
     v, buffer = cv2.imencode(".jpg", resized)
     img_str = base64.b64encode(buffer).decode('utf-8')
@@ -16,7 +18,7 @@ def img2vec(img):
     url = "http://localhost:8080/api/genhog"
 
     
-    
+    #แบบ body json
     response = requests.get(url, json={"img_data":img_data_string})
     
     if response.status_code == 200:
@@ -28,23 +30,27 @@ def img2vec(img):
         print("API Request Error. Status Code:", response.status_code)
         return None
 
+#path รูปภาพทั้ง train และ test
 data_path="Cars Dataset/train"
 # data_path="Cars Dataset/test"
-X=[]
-Y=[]
 
+
+X=[]#สร้าง X เพื่อเก็บรูปภาพรถยนต์ทั้งหมด 
+Y=[]#สร้าง Y เพื่อเก็บยี่ห้อรถยนต์จากชื่อของfolderทั้งหมด 
+
+#ทำการเก็บ X Y จากloopนี้
 for sub in os.listdir(data_path):
     sub_path = os.path.join(data_path, sub)
     if os.path.isdir(sub_path):
         for fn in os.listdir(sub_path):   
-            if not fn.startswith('.'):
+            if not fn.startswith('.'): #ไม่เอา hidden file มาด้วย
                 img_file_name = os.path.join(sub_path, fn)
                 img = cv2.imread(img_file_name)
                 X.append(img)
                 Y.append(sub)
 
+#เก็บค่า hog ที่ได้มาทั้งหมดจากfunction img2vec
 HOGVectors=[]
-
 for i in range(len(X)):
     try:
         res = img2vec(X[i])
@@ -57,6 +63,7 @@ for i in range(len(X)):
     except Exception as e:
         print("Error processing image:", e)
 
+#ทำการบันทึกทั้งของtrain และ testเพื่อไปใช้ generate model
 write_path_train="hogvectors_train.pkl"
 # write_path_test="hogvectors_test.pkl"
 
--- genmodel.py
+++ genmodel.py
@@ -7,35 +7,37 @@ from sklearn.metrics import accuracy_score,confusion_matrix
 import xgboost as xgb
 from sklearn.ensemble import VotingClassifier  
 
-
+#load pickle train และ test
 data_train=pickle.load(open(r'hogvectors_train.pkl','rb'))
 data_test=pickle.load(open(r'hogvectors_test.pkl','rb'))
+
+#สร้างตัวแปรมาเก็บค่า
 X_data_train=[]
 Y_data_train=[]
 X_data_test=[]
 Y_data_test=[]
 
-X_data_train = [entry[:8100] for entry in data_train]
-Y_data_train = [entry[8100] for entry in data_train]
+
+X_data_train = [entry[:8100] for entry in data_train]#เก็บเอาค่าตั้งแต่ 0-8100
+Y_data_train = [entry[8100] for entry in data_train]#เก็บอันสุดท้ายมาไว้
 label_encoder_train = LabelEncoder()
-Y_numeric_train = label_encoder_train.fit_transform(Y_data_train)
+Y_numeric_train = label_encoder_train.fit_transform(Y_data_train)#แปลงยี่ห้อรถเป็น label
 
 
-X_data_test = [entry[:8100] for entry in data_test]
-Y_data_test = [entry[8100] for entry in data_test]
-label_encoder_test = LabelEncoder()
+X_data_test = [entry[:8100] for entry in data_test]#เก็บเอาค่าตั้งแต่ 0-8100
+Y_data_test = [entry[8100] for entry in data_test]#เก็บเอาค่าตั้งแต่ 0-8100
+label_encoder_test = LabelEncoder()#แปลงยี่ห้อรถเป็น label
 Y_numeric_test = label_encoder_test.fit_transform(Y_data_test)
 
-
-# X_train, X_test, y_train, y_test = train_test_split(X_data_train, Y_numeric_train, test_size=0.2)
+#สร้างmodel decision tree และ xgboost
 model_tree = DecisionTreeClassifier(random_state=42)
 model_xgb = xgb.XGBClassifier(objective="multi;softmax",num_class=len(Y_numeric_train),random_state=42)
 
-
-# Decision tree and XGBoost together using Voting Classifier
+#ใช้ votingเพื่อใช้ 2 model ร่วมกัน (ensemble)
 ensemble_model = VotingClassifier(estimators=[('decision_tree', model_tree), ('xgb', model_xgb)], voting='hard',weights=[1,4])
 ensemble_model.fit(X_data_train, Y_numeric_train)
 y_pred = ensemble_model.predict(X_data_test)
+#คะแนน accuracy และ confusion matrix
 accuracy = accuracy_score(Y_numeric_test, y_pred)
 print("Ensemble Accuracy:", accuracy)
 confusion_mat=confusion_matrix(Y_numeric_test,y_pred)
@@ -44,6 +46,8 @@ print(confusion_mat)
 write_path_model="model.pkl"
 pickle.dump(ensemble_model,open(write_path_model,"wb"))
 print("done")
+
+
 # Only Decision Tree #
 # model_tree = model_tree.fit(X_data_train,Y_numeric_train)
 # y_pred = model_tree.predict(X_data_test)
